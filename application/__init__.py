1d62060b32607050812f68c9a311ec1eaf6e7e55
unknown <tomibami2020@gmail.com>
2021-06-05 21:18:24 +0100

    updated __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -11,12 +11,12 @@ def create_app():
     app = Flask(__name__)
 
     config = load_config()
-    app.config["SECRET_KEY"] = config["SECRET_KEY"]
-    app.config["MYSQL_DATABASE_DB"] = config["DB"]["DATABASE"]
-    app.config["MYSQL_DATABASE_USER"] = config["DB"]["USERNAME"]
-    app.config["MYSQL_DATABASE_PASSWORD"] = config["DB"]["PASSWORD"]
+    # app.config["SECRET_KEY"] = config["SECRET_KEY"]
+    # app.config["MYSQL_DATABASE_DB"] = config["DB"]["DATABASE"]
+    # app.config["MYSQL_DATABASE_USER"] = config["DB"]["USERNAME"]
+    # app.config["MYSQL_DATABASE_PASSWORD"] = config["DB"]["PASSWORD"]
 
-    mysql.init_app(app)
+    # mysql.init_app(app)
     # blueprint for auth routes in our app
     from .api import api
     from .main import main
