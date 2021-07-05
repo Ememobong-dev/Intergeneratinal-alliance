4eddf8d5bccf67bc55d0b92b50c75cbd97b0ba7e
Ememobong-dev <Ememobongumoren@gmail.com>
2021-07-05 22:05:04 +0100

    ...

---
+++ application/static/admin/js/requestApi.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById('admin_forms');
+
+loginForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const formData = new formData(this)
+
+    fetch('http://127.0.0.1:5000/api/login', {
+        method: 'POST',
+        body: FormData
+    }).then(function (response) {
+        return response.text();
+    }).then(function (text) {
+        console.log(text);
+    }).catch(function (error) {
+        console.error(error);
+        
+    })
+    
+})
